3b2269fc0c11bfb224eaf9f506ff3fbdb03cba57
Marinadosan <72498316+Marinadosan@users.noreply.github.com>
2020-10-07 12:56:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//alert ("Mariano");
+//alert ("Rata");
+var nombre = "Marinao";
+var altura = 175;
+var concatenacion = nombre + " " + altura
+
+document.write(concatenacion);
+//document.write(nombre);
+//document.write(altura);
+document.getElementById("datos");
+innerHTML = concatenacion;
